1675e349d658e476f4fcbe81065559c5dcde103e
Nicholas Wang <nwang@MacBook-Pro-9.localdomain>
2019-10-29 14:07:02 -0400

    create new records

--- hermite.js
+++ hermite.js
@@ -20,18 +20,18 @@ img.crossOrigin = "Anonymous"; //cors support
 img.onload = function(){
 	
 	// window.orientation = 1;
-	// EXIF.getData(img, function() {
- //        orientation = EXIF.getTag(this, "Orientation");
+	EXIF.getData(img, function() {
+        orientation = EXIF.getTag(this, "Orientation");
+        alert(orientation);
+       	// window.onorientationchange = readDeviceOrientation();
+       	// console.log(window.orientation);
+       	// alert(window.orientation);
+       	if (orientation == 6 ){ // || window.orientation == 0 ) {
+       		// alert("Hello");
+		canvas.className = 'element';
 
- //       	// window.onorientationchange = readDeviceOrientation();
- //       	// console.log(window.orientation);
- //       	// alert(window.orientation);
- //       	if (orientation == 6 ){ // || window.orientation == 0 ) {
- //       		// alert("Hello");
-	// 	canvas.className = 'element';
-
-	// }
- //    });
+	}
+    });
 
     var resize_size = 10; //1-100
 	resize(resize_size, img, canvas, ctx, HERMITE);
